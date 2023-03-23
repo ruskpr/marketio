02d6d1b5dac48864e9ac9b08f4e25ea2ea5c5de5
Rustu Koprulu <rustulp@gmail.com>
2023-03-22 21:33:33 -0600

    added

---
+++ application/MarketIO/MarketIO/Shared/Helpers/SHA256.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketIO.Shared.Helpers
+{
+    public class SHA256
+    {
+        public static string Hash(string input)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
