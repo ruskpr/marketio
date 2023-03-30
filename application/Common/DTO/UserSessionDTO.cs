05789d08d6c9026910131068dce4c66dd290ef60
Rustu Koprulu <rustulp@gmail.com>
2023-03-30 00:11:29 -0600

    updated

---
+++ application/Common/DTO/UserSessionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTO
+{
+    public class UserSessionDTO
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public int ExpiresIn { get; set; }
+        public DateTime ExpiryTimeStamp { get; set; }
+    }
+}
--- application/marketioBlazor/Program.cs
+++ application/marketioBlazor/Program.cs
@@ -1,13 +1,18 @@
+using Blazored.SessionStorage;
 using Common;
+using marketioBlazor.Authentication;
 using marketioBlazor.Data;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredSessionStorage(); // added
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>(); // added
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<ApiClientService>();
+builder.Services.AddSingleton<ApiClientService>(); // added
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
