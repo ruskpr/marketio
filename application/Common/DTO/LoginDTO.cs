6db508550aa47505422d1b817fb15e7182a1b006
Rustu Koprulu <rustulp@gmail.com>
2023-04-03 16:56:01 -0600

    implement validation

--- application/Common/DTO/LoginDTO.cs
+++ application/Common/DTO/LoginDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace Common.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.DTO
 {
     public class LoginDTO
     {
+        [Required]
+        [Display(Name = "email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "password")]
         public string PasswordHash { get; set; }
     }
 }
--- application/Common/DTO/RegisterDTO.cs
+++ application/Common/DTO/RegisterDTO.cs
@@ -5,23 +5,30 @@ namespace Common.DTO
     public class RegisterDTO
     {
         [Required]
-        [MaxLength(100), MinLength(3)]
+        [MaxLength(100), MinLength(3, ErrorMessage = "Email must be more the 2 characters.")]
+        [Display(Name = "email")]
         public string Email { get; set; }
 
         [Required]
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "First name must be less than 50 characters.")]
+        [Display(Name = "first name")]
         public string FirstName { get; set; }
 
         [Required]
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "Last name must be less than 50 characters.")]
+        [Display(Name = "last name")]
         public string LastName { get; set; }
 
         [Required]
         [MaxLength(64)]
+        [Display(Name = "password")]
         public string PasswordHash { get; set; }
 
         [Required]
         [MaxLength(64)]
+        [Display(Name = "confirm password")]
+        [Compare("PasswordHash",
+        ErrorMessage = "password fields do not match.")]
         public string ConfirmPasswordHash { get; set; }
 
         [Required]
