c657926f9a7696db7c98abdd87efea1c8157b0a5
Rustu Koprulu <rustulp@gmail.com>
2023-04-04 08:30:12 -0600

    added

---
+++ application/Common/Helpers/ImageConvert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Helpers
+{
+    public class ImageConvert
+    {
+        // create a method that converts a byte array to base64
+        public static string ConvertByteArrayToBase64(byte[] imageBytes)
+        {
+            return $"data:image/png;base64,{Convert.ToBase64String(imageBytes)}";
+        }
+
+    }
+}
