f557fbf461e1147ced39776b4d8c06022b1da357
Rustu Koprulu <rustulp@gmail.com>
2023-04-05 23:49:42 -0600

    updated

---
+++ application/marketioWebAPI/Migrations/20230406054126_removeListingTags.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace marketioWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeListingTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListingTags");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TagString",
+                table: "Listings",
+                type: "varchar(50)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TagString",
+                table: "Listings");
+
+            migrationBuilder.CreateTable(
+                name: "ListingTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ListingId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListingTags", x => x.Id);
+                });
+        }
+    }
+}
